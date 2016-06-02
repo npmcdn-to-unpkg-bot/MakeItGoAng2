a217ed9de1376d3f62664f6644c5e399854289fd
Raphael <raphael.thiney@gmail.com>
2016-06-02 11:15:40 -0400

    init 2015 Ang2 Starter

--- src/MakeItGoAng2/Startup.cs
+++ src/MakeItGoAng2/Startup.cs
@@ -20,10 +20,9 @@ namespace MakeItGoAng2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
         }
     }
 }
